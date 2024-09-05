084ec6ddc8edefe68cb395b94043daf6daa8b1ff
Thomas M <mail@lokalnetz.com>
2024-09-05 09:29:16 +0200

    add simple 24h clock

--- component.js
+++ component.js
@@ -1,7 +1,22 @@
-const Component = ({ content }) => {
+const Component = ({ clock = false }) => {
+
+    if (!clock) {
+        return false;
+    }
+
+    const [time, setTime] = React.useState(new Date());
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return (
-        <>
-            {content}
-        </>
+        <h2>
+            {time.toLocaleTimeString('de-DE', { hour12: false })}
+        </h2>
     );
-}
+};
